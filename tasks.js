f1ee0a1a3087a5fa58c6459200bd6cea94d5aab7
Francisco Barciella <barciella@gmail.com>
2016-07-13 18:55:32 -0300

    Checking Spot Price

--- tasks.js
+++ tasks.js
@@ -1,49 +1,61 @@
 
 var AWS = require('aws-sdk'),
   uuid = require('uuid'),
+  moment = require('moment'),
   btoa = require('btoa');
 
 function tasksController(){
   var that = this;
   var ec2 = new AWS.EC2({region: 'sa-east-1'});
   that.jobs = {};
-  that.spotInstancesIds = {};
+  var date = moment();
 
-  //that.lastSpotInstanceRequestId = [];
-
-//Get all jobs
+  //Get all jobs
   that.get = function(req, res, next) {
     res.send(200, that.jobs);
     return next();
   }
 
   that.post = function(req, res, next){
-    idAuto = uuid.v4();
+    idAuto = uuid.v4(); //generate uuid
     that.jobs[idAuto] = {};
-    that.jobs[idAuto].LaunchSpecification = req.body.paramsEnv;
-    that.jobs[idAuto].dados = req.body.UserData;
-  //  console.log(req.body.UserData);
-    that.jobs[idAuto].base64 = btoa(req.body.UserData);
-    console.log(that.jobs[idAuto].base64);
-    //that.jobs[idAuto].teste = new Date();
+    that.jobs[idAuto].LaunchSpecification = req.body.paramsEnv; //get paramsEnv from Json
+    that.jobs[idAuto].LaunchSpecification.UserData = btoa(that.jobs[idAuto].LaunchSpecification.UserData); //transform user data in base64 so aws can accept
 
-    var params = {
-      LaunchSpecification: that.jobs[idAuto].LaunchSpecification,
-      UserData: toString(that.jobs[idAuto].base64),
-      SpotPrice: "0.002",
-    };
-
-    ec2.requestSpotInstances(params, function(error, data){
-      if (error) {
+    //get spot price for instance_id
+    var paramsPrice = {
+      AvailabilityZone: "sa-east-1a",
+      InstanceTypes: [that.jobs[idAuto].LaunchSpecification.InstanceType],
+      EndTime: date.toISOString(date),
+      StartTime: date.toISOString(date.subtract(7, "days")),
+      ProductDescriptions: ["Linux/Unix"]
+    }
+    ec2.describeSpotPriceHistory(paramsPrice, function(error, data){
+      if(error){
         console.log(error);
+        console.log(paramsPrice);
       } else {
         console.log(data);
-        that.jobs[idAuto].SpotInstanceRequests = data.SpotInstanceRequests;
       }
     });
-    res.send(201);
-  };
 
+    //launch the Spot InstanceType
+  //  var params = {
+    //  LaunchSpecification: that.jobs[idAuto].LaunchSpecification,
+      //comment so it can crash //SpotPrice: "0.002",
+      //ProductDescription: "Linux/Unix"
+    //};
+
+
+  //  ec2.requestSpotInstances(params, function(error, data){
+  //    if (error) {
+//        console.log(error);
+//      } else {
+//        that.jobs[idAuto].SpotInstanceRequests = data.SpotInstanceRequests;
+//    });
+//    res.send(201);
+//  };
+}
   //Get specific jobs
   that.getById = function(req, res, next){
     var oJob = that.jobs[ req.params.id ];
