a22d67c03bdf737dcb877008ee42fff04068551a
Francisco Barciella <Francisco Barciella>
2016-07-13 23:34:02 -0300

    setting final prices

--- tasks.js
+++ tasks.js
@@ -9,7 +9,7 @@ function tasksController(){
   var ec2 = new AWS.EC2({region: 'sa-east-1'});
   that.jobs = {};
   var date = moment();
-
+  var price = "";
   //Get all jobs
   that.get = function(req, res, next) {
     res.send(200, that.jobs);
@@ -20,41 +20,46 @@ function tasksController(){
     idAuto = uuid.v4(); //generate uuid
     that.jobs[idAuto] = {};
     that.jobs[idAuto].LaunchSpecification = req.body.paramsEnv; //get paramsEnv from Json
-    that.jobs[idAuto].LaunchSpecification.UserData = btoa(that.jobs[idAuto].LaunchSpecification.UserData); //transform user data in base64 so aws can accept
+    that.jobs[idAuto].LaunchSpecification.UserData = "I2Nsb3VkLWNvbmZpZw0KcmVwb191cGRhdGU6IHRydWUNCnJlcG9fdXBncmFkZTogYWxsDQoNCnBhY2thZ2VzOg0KIC0gaHR0cGQyNA0KIC0gcGhwNTYNCiAtIG15c3FsNTUNCiAtIHNlcnZlcg0KIC0gcGhwNTYtbXlzcWxuZA0KDQpydW5jbWQ6DQogLSBzZXJ2aWNlIGh0dHBkIHN0YXJ0DQogLSBjaGtjb25maWcgaHR0cGQgb24NCiAtIGdyb3VwYWRkIHd3dw0KIC0gWyBzaCwgLWMsICJ1c2VybW9kIC1hIC1HIHd3dyBlYzItdXNlciIgXQ0KIC0gWyBzaCwgLWMsICJjaG93biAtUiByb290Ond3dyAvdmFyL3d3dyIgXQ0KIC0gY2htb2QgMjc3NSAvdmFyL3d3dw0KIC0gWyBmaW5kLCAvdmFyL3d3dywgLXR5cGUsIGQsIC1leGVjLCBjaG1vZCwgMjc3NSwge30sICsgXQ0KIC0gWyBmaW5kLCAvdmFyL3d3dywgLXR5cGUsIGYsIC1leGVjLCBjaG1vZCwgMDY2NCwge30sICsgXQ0KIC0gWyBzaCwgLWMsICdlY2hvICI8P3BocCBwaHBpbmZvKCk7ID8+IiA+IC92YXIvd3d3L2h0bWwvcGhwaW5mby5waHAnIF0="; //auto test
+    //that.jobs[idAuto].LaunchSpecification.UserData = btoa(that.jobs[idAuto].LaunchSpecification.UserData); //transform user data in base64 so aws can accept
 
     //get spot price for instance_id
     var paramsPrice = {
       AvailabilityZone: "sa-east-1a",
       InstanceTypes: [that.jobs[idAuto].LaunchSpecification.InstanceType],
-      EndTime: date.toISOString(date),
-      StartTime: date.toISOString(date.subtract(7, "days")),
-      ProductDescriptions: ["Linux/Unix"]
+      EndTime: date.toISOString(),
+      StartTime: date.toISOString(date.subtract(15, "minutes")),
+      ProductDescriptions: ["Linux/UNIX"]
     }
-    ec2.describeSpotPriceHistory(paramsPrice, function(error, data){
-      if(error){
-        console.log(error);
-        console.log(paramsPrice);
-      } else {
-        console.log(data);
-      }
-    });
+    // ec2.describeSpotPriceHistory(paramsPrice, function(error, data){
+    //   if(error){
+    //     console.log(error);
+    //   } else {
+    //     console.log(data);
+    //     var price1 = data.SpotPriceHistory.SpotPrice;
+    //     // price1(data.SpotPriceHistory.SpotPrice);
+    //     // price = data.SpotPriceHistory.SpotPrice;
+    //     // // return price;
+    //      console.log(price1);
+    //   }
+    // });
 
     //launch the Spot InstanceType
-  //  var params = {
-    //  LaunchSpecification: that.jobs[idAuto].LaunchSpecification,
-      //comment so it can crash //SpotPrice: "0.002",
-      //ProductDescription: "Linux/Unix"
-    //};
+   var params = {
+     LaunchSpecification: that.jobs[idAuto].LaunchSpecification,
+      SpotPrice: "0.001"
+    };
 
 
-  //  ec2.requestSpotInstances(params, function(error, data){
-  //    if (error) {
-//        console.log(error);
-//      } else {
-//        that.jobs[idAuto].SpotInstanceRequests = data.SpotInstanceRequests;
-//    });
-//    res.send(201);
-//  };
+   ec2.requestSpotInstances(params, function(error, data){
+     if (error) {
+       console.log(error);
+     } else {
+       console.log(data);
+       //that.jobs[idAuto].SpotInstanceRequests = data.SpotInstanceRequests;
+   }
+   res.send(201);
+ });
 }
   //Get specific jobs
   that.getById = function(req, res, next){
