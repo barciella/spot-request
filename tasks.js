a476701b1e6217197387fdc9ec7a18960d1e97db
Francisco Barciella <barciella@gmail.com>
2016-07-14 15:36:25 -0300

    Whoa, cloud-init super string works

--- tasks.js
+++ tasks.js
@@ -16,19 +16,40 @@ function tasksController(){
     return next();
   }
 
+//post function
   that.post = function(req, res, next){
     idAuto = uuid.v4(); //generate uuid
     that.jobs[idAuto] = {};
     that.jobs[idAuto].LaunchSpecification = req.body.paramsEnv; //get paramsEnv from Json
-    that.jobs[idAuto].LaunchSpecification.UserData = "I2Nsb3VkLWNvbmZpZw0KcmVwb191cGRhdGU6IHRydWUNCnJlcG9fdXBncmFkZTogYWxsDQoNCnBhY2thZ2VzOg0KIC0gaHR0cGQyNA0KIC0gcGhwNTYNCiAtIG15c3FsNTUNCiAtIHNlcnZlcg0KIC0gcGhwNTYtbXlzcWxuZA0KDQpydW5jbWQ6DQogLSBzZXJ2aWNlIGh0dHBkIHN0YXJ0DQogLSBjaGtjb25maWcgaHR0cGQgb24NCiAtIGdyb3VwYWRkIHd3dw0KIC0gWyBzaCwgLWMsICJ1c2VybW9kIC1hIC1HIHd3dyBlYzItdXNlciIgXQ0KIC0gWyBzaCwgLWMsICJjaG93biAtUiByb290Ond3dyAvdmFyL3d3dyIgXQ0KIC0gY2htb2QgMjc3NSAvdmFyL3d3dw0KIC0gWyBmaW5kLCAvdmFyL3d3dywgLXR5cGUsIGQsIC1leGVjLCBjaG1vZCwgMjc3NSwge30sICsgXQ0KIC0gWyBmaW5kLCAvdmFyL3d3dywgLXR5cGUsIGYsIC1leGVjLCBjaG1vZCwgMDY2NCwge30sICsgXQ0KIC0gWyBzaCwgLWMsICdlY2hvICI8P3BocCBwaHBpbmZvKCk7ID8+IiA+IC92YXIvd3d3L2h0bWwvcGhwaW5mby5waHAnIF0="; //auto test
-    //that.jobs[idAuto].LaunchSpecification.UserData = btoa(that.jobs[idAuto].LaunchSpecification.UserData); //transform user data in base64 so aws can accept
+    that.jobs[idAuto].LaunchSpecification.UserData = `#cloud-config
+repo_update: true
+repo_upgrade: all
 
-    //get spot price for instance_id
+packages:
+ - httpd24
+ - php56
+ - mysql55
+ - server
+ - php56-mysqlnd
+
+runcmd:
+ - service httpd start
+ - chkconfig httpd on
+ - groupadd www
+ - [ sh, -c, "usermod -a -G www ec2-user" ]
+ - [ sh, -c, "chown -R root:www /var/www" ]
+ - chmod 2775 /var/www
+ - [ find, /var/www, -type, d, -exec, chmod, 2775, {}, + ]
+ - [ find, /var/www, -type, f, -exec, chmod, 0664, {}, + ]
+ - [ sh, -c, 'echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php' ]` + req.body.DockerID; //auto test, jesus christ it works. req.
+    that.jobs[idAuto].LaunchSpecification.UserData = btoa(that.jobs[idAuto].LaunchSpecification.UserData); //transform user data in base64 so aws can accept
+
+    //get spot price for instance_id and use that price to launch the ec2
     var paramsPrice = {
       AvailabilityZone: "us-west-2a",
       InstanceTypes: [that.jobs[idAuto].LaunchSpecification.InstanceType],
       EndTime: date.toISOString(),
-      StartTime: date.toISOString(date.subtract(15, "minutes")),
+      StartTime: date.subtract(30, "minutes").toISOString(),
       ProductDescriptions: ["Linux/UNIX"]
     }
     ec2.describeSpotPriceHistory(paramsPrice, function(error, data){
@@ -37,38 +58,42 @@ function tasksController(){
       } else {
         console.log(data);
         that.jobs[idAuto].SpotPrice = data.SpotPriceHistory[0].SpotPrice;
-        console.log("preco eh" + price1);
-        // price1(data.SpotPriceHistory.SpotPrice);
-        // price = data.SpotPriceHistory.SpotPrice;
-        // // return price;
-
+        //launch the Spot InstanceType request
+         var params = {
+           LaunchSpecification: that.jobs[idAuto].LaunchSpecification,
+           SpotPrice: that.jobs[idAuto].SpotPrice
+           };
+          ec2.requestSpotInstances(params, function(error, data){
+           if (error) {
+             console.log(error);
+           } else {
+             console.log(data);
+             that.jobs[idAuto].SpotRequest = data;
+             //tag the instance with generated autoid
+             var params = {
+                Resources: [that.jobs[idAuto].SpotRequest.SpotInstanceRequests[0].SpotInstanceRequestId],
+                Tags: [{Key: "idAuto", Value: idAuto}]
+            }
+             ec2.createTags(params, function(error, data){
+               if (error) {
+                 console.log(error);
+               } else {
+                 console.log(data);
+               }
+             });
+          }
+         res.send(201);
+       });
       }
     });
-
-    //launch the Spot InstanceType
-   var params = {
-     LaunchSpecification: that.jobs[idAuto].LaunchSpecification,
-     SpotPrice: that.jobs[idAuto].SpotPrice
-     };
-
-
-   ec2.requestSpotInstances(params, function(error, data){
-     if (error) {
-       console.log(error);
-     } else {
-       console.log(data);
-       //that.jobs[idAuto].SpotInstanceRequests = data.SpotInstanceRequests;
-   }
-   res.send(201);
- });
 }
   //Get specific jobs
   that.getById = function(req, res, next){
-    var oJob = that.jobs[ req.params.id ];
-    if ( oJob ) {
+    var oJob = that.jobs[req.params.id];
+    if (oJob) {
       res.send(200, oJob);
-    } else {
-      res.send(404, "Task not found");
+    }else{
+      res.send(404, "Job Not Found");
     }
     return next();
   }
