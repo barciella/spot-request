e6ca1a702b03dd7a0beb9d3b3a08da4fc446b00f
Francisco Barciella <Francisco Barciella>
2016-07-16 18:32:54 -0300

    testing new script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-set -e -x
-export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get upgrade -y
-tasksel install lamp-server
-echo "Please remember to set the MySQL root password!"
+yum install lamp /y
