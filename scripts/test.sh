a6438ccf0a0a5922f1e8e921ef64075c72a121f4
Francisco Barciella <Francisco Barciella>
2016-07-16 20:41:36 -0300

    get metadata tag for future scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ping www.google.com
+INSTANCE_ID=`wget -qO- http://instance-data/latest/meta-data/instance-id`
+REGION=`wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed 's/.$//'`
+aws ec2 describe-tags --region $REGION --filter "Name=resource-id,Values=$INSTANCE_ID" --output=text | sed -r 's/TAGS\t(.*)\t.*\t.*\t(.*)/\1="\2"/' > /etc/ec2-tags
